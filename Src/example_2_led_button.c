5c791d4b81047b7f86e4b187ac04d5dec25369b9
Kacper Janowski <kacper.janowski98@wp.pl>
2021-10-02 17:19:09 +0200

    Test 02.

---
+++ Src/example_2_led_button.c
@@ -0,0 +1,54 @@
+/*
+ * example_2_led_button.c
+ *
+ *  Created on: 2 paź 2021
+ *      Author: Kacper Janowski
+ */
+
+#include "stm32f407xx.h"
+
+#define HIGH 1
+#define BTN_PRESSED HIGH
+
+void delay(void)
+{
+	for(uint32_t i = 0; i < 500000/2; i ++);
+}
+
+int main(void)
+{
+
+	GPIO_Handle_t GpioLed, GPIOBtn;
+
+	// this is led gpio configuration
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OP;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOD, ENABLE);
+	GPIO_Init(&GpioLed);
+
+	// this is btn gpio configuration
+	GPIOBtn.pGPIOx = GPIOA;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOA, ENABLE);
+	GPIO_Init(&GPIOBtn);
+
+	while(1)
+	{
+		if(GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0) == BTN_PRESSED)
+		{
+			delay();
+			GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+		}
+	}
+
+	return 0;
+}
