18ddf62174e55003aa0650fd22103f8a8899da9a
Kacper Janowski <kacper.janowski98@wp.pl>
2021-10-02 11:27:02 +0200

    Developed GPIO_Init.

--- Drivers/Inc/gpio_drivers.h
+++ Drivers/Inc/gpio_drivers.h
@@ -29,6 +29,68 @@ typedef struct
 	GPIO_PinConfig_t GPIO_PinConfig;		/*!< This holds GPIO pin configuration settings >*/
 }GPIO_Handle_t;
 
+/*
+ *  @GPIO_PIN_NUMBER
+ *  GPIO pin numbers
+*/
+
+#define GPIO_PIN_NO_0				0
+#define GPIO_PIN_NO_1				1
+#define GPIO_PIN_NO_2				2
+#define GPIO_PIN_NO_3				3
+#define GPIO_PIN_NO_4				4
+#define GPIO_PIN_NO_5				5
+#define GPIO_PIN_NO_6				6
+#define GPIO_PIN_NO_7				7
+#define GPIO_PIN_NO_8				8
+#define GPIO_PIN_NO_9				9
+#define GPIO_PIN_NO_10				10
+#define GPIO_PIN_NO_11				11
+#define GPIO_PIN_NO_12				12
+#define GPIO_PIN_NO_13				13
+#define GPIO_PIN_NO_14				14
+#define GPIO_PIN_NO_15				15
+
+/*
+ *  @GPIO_PIN_MODES
+ *  GPIO pin possible modes
+ */
+
+#define GPIO_MODE_IN 				0 // input
+#define GPIO_MODE_OUT 				1 // output
+#define GPIO_MODE_ALTFN 			2 // alternative function
+#define GPIO_MODE_ANALOG 			3 // analog
+#define GPIO_MODE_IT_FT				4 // interrupt falling edge trigger
+#define GPIO_MODE_IT_RT				5 // interrupt raising edge trigger
+#define GPIO_MODE_IT_RFT			6 // interrupt raising/falling edge trigger
+
+/*
+ *  @GPIO_PIN_OUTPUT_PULL_UP_DOWN
+ *  GPIO pin possible output types
+*/
+
+#define GPIO_OP_TYPE_PP  			0 // output push pull
+#define GPIO_OP_TYPE_OP  			1 // output open drain
+
+/*
+ *  @GPIO_PIN_SPEED
+ *  GPIO pin possible output speeds
+*/
+
+#define GPIO_SPEED_LOW				0
+#define GPIO_SPEED_MEDIOM			1
+#define GPIO_SPEED_FAST				2
+#define GPIO_SPEED_HIGH				3
+
+/*
+ *   @GPIO_PIN_PULL_UP_DOWN
+ *  GPIO pin pull up pull down configuration macros
+*/
+
+#define GPIO_NO_PUPD				0 // No pull-up, pull-down
+#define GPIO_PIN_PU					1 // pull-up
+#define GPIO_PIN_PD					2 // pull-down
+
 /***************************************************************************************************
  * 									APIs supported by this driver
  * 				For more information about the APIs check the function definitions
--- Drivers/Src/gpio_drivers.c
+++ Drivers/Src/gpio_drivers.c
@@ -79,3 +79,70 @@ void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
 		}
 	}
 }
+
+/**********************************************************************************************************
+ * @fn 				- GPIO_Init
+ *
+ * #brief 			- This function set specific parameters for GPIOx (Mode, Speed, input/output etc.)
+ * pGPIOHandle		- This is structure which contain: base address of the gpio peripheral and pin configuration
+ *
+ *********************************************************************************************************/
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
+{
+	uint32_t temp = 0; // temp. register
+
+	//1. Configure the mode of GPIO Pin
+
+	if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
+	{
+		temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+		pGPIOHandle->pGPIOx->MODER &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber); // clear
+		pGPIOHandle->pGPIOx->MODER |= temp; // set
+	} else
+	{
+		// interrupt mode
+	}
+
+	temp = 0;
+
+	//2. Configure the speed
+
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->OSPEEDR &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber); // clear
+	pGPIOHandle->pGPIOx->OSPEEDR |= temp; // set
+
+	temp = 0;
+
+	//3. Configure the pudp settings
+
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+	pGPIOHandle->pGPIOx->PUPDR &= ~(0x3 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber); // clear
+	pGPIOHandle->pGPIOx->PUPDR |= temp; // set
+
+	temp = 0;
+
+	//4. Configure the optype
+
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+	pGPIOHandle->pGPIOx->OTYPER &= ~(0x1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber); // clear
+	pGPIOHandle->pGPIOx->OTYPER |= temp; // set
+
+	temp = 0;
+
+	//5. Configure the alt functionality
+
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN)
+	{
+		// configure the alt function register.
+		uint32_t temp1, temp2;
+
+		temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 8;		// defined register
+		temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 8;		// defined number of pin
+		pGPIOHandle->pGPIOx->AFR[temp1] &= ~(0xF << (4 * temp2)); 	// clear
+		pGPIOHandle->pGPIOx->AFR[temp1] |= (pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << (4 * temp2)); // set
+	}
+
+}
+
+
+
