3e779eac077a7a18a44a170ab7bacaa159fbc2cb
Kacper Janowski <kacper.janowski98@wp.pl>
2021-10-02 19:50:52 +0200

    Fix.

--- Drivers/Src/gpio_drivers.c
+++ Drivers/Src/gpio_drivers.c
@@ -332,13 +332,15 @@ void GPIO_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi)
  * IRQPriority 		- Number of priority
  *
  *********************************************************************************************************/
-void GPIO_IRQPriorityConfig (uint8_t IRQNumber, uint8_t IRQPriority)
+void GPIO_IRQPriorityConfig (uint8_t IRQNumber, uint32_t IRQPriority)
 {
 	//1. First lets find out the ipr register
 	uint8_t iprx = IRQNumber / 4;
 	uint8_t iprx_section = IRQNumber % 4;
 
-	*(NVIC_PR_BASE_ADDR + (iprx * 4)) |= IRQPriority << (8 * iprx_section);
+	uint8_t shift_amount = (8 * iprx_section) + (8 - NO_PR_BITS_IMPLEMENTED);
+
+	*(NVIC_PR_BASE_ADDR + iprx) |= (IRQPriority << shift_amount);
 }
 
 /**********************************************************************************************************
