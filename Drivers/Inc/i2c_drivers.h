2133c7b279de23df9904d3dbf8d5be4f29b88229
Kacper Janowski <kacper.janowski98@wp.pl>
2021-11-09 09:43:32 +0100

    Added structures for config and handle I2C.

--- Drivers/Inc/i2c_drivers.h
+++ Drivers/Inc/i2c_drivers.h
@@ -8,6 +8,26 @@
 #ifndef INC_I2C_DRIVERS_H_
 #define INC_I2C_DRIVERS_H_
 
+#include "stm32f407xx.h"
 
+/**
+ * Configuration structure for I2Cx peripheral
+ */
+typedef struct
+{
+	uint32_t I2C_SCLSpeed;
+	uint8_t I2C_DeviceAddress;
+	uint8_t I2C_AckControl;
+	uint8_t I2C_FMDutyCycle;
+} I2C_Config_t;
+
+/**
+ * Handle structure for I2Cx peripheral
+ */
+typedef struct
+{
+	I2C_RegDef_t *pI2Cx;
+	I2C_Config_t I2C_Config;
+} I2C_Handle_t;
 
 #endif /* INC_I2C_DRIVERS_H_ */
