09ace9887a4fc8431c97ebc4e9e3094365b8af0b
Kacper Janowski <kacper.janowski98@wp.pl>
2021-11-09 10:00:46 +0100

    Added definitions for I2C API.

--- Drivers/Inc/i2c_drivers.h
+++ Drivers/Inc/i2c_drivers.h
@@ -49,4 +49,42 @@ typedef struct
 #define I2C_SFM_DUTY_2			0
 #define I2C_SFM_DUTY_16_9		1
 
+/****************************************************************************************************************
+ *										 APIs supported by this driver
+ * 						For more information about the APIs check the function definitions
+ * *************************************************************************************************************/
+
+/**
+ * Peripheral Clock setup
+ */
+void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);
+
+/**
+ * Unit and De-init
+ */
+void I2C_Init(I2C_Handle_t *pI2CHandle);
+void I2C_DeInit(I2C_RegDef_t *pI2Cx);
+
+/**
+ * Data Send and Receive
+ */
+
+
+/**
+ * IRQ Configuration and ISR handling
+ */
+void I2C_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void I2C_IRQPriorityConfig (uint8_t IRQNumber, uint32_t IRQPriority);
+
+/**
+ * Other Peripheral Control APIs
+ */
+uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t FlagName);
+void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
+
+/**
+ * Application callback
+ */
+void I2C_ApplicationEventCallback(I2C_Handle_t *pI2CHandle, uint8_t AppEv);
+
 #endif /* INC_I2C_DRIVERS_H_ */
