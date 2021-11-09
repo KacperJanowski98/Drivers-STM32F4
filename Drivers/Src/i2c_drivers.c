5b896e06767656ab1eb62a23b6c34bfbdc51d173
Kacper Janowski <kacper.janowski98@wp.pl>
2021-11-09 10:09:07 +0100

    Developed peripheral clock enable function for I2C.

--- Drivers/Src/i2c_drivers.c
+++ Drivers/Src/i2c_drivers.c
@@ -5,4 +5,41 @@
  *      Author: Kacper
  */
 
+#include "i2c_drivers.h"
 
+/**********************************************************************************************************
+ * @fn 				- I2C_PeriClockControl
+ *
+ * #brief 			- This function enables or disables peripheral clock for given I2C port
+ * pSPIx			- Base address of the I2C peripheral
+ * EnorDi 			- ENABLE or DISABLE macros
+ *
+ *********************************************************************************************************/
+void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
+{
+	if (EnorDi == ENABLE)
+	{
+		if (pI2Cx == I2C1)
+		{
+			I2C1_PCLK_EN();
+		} else if (pI2Cx == I2C2)
+		{
+			I2C2_PCLK_EN();
+		} else if (pI2Cx == I2C3)
+		{
+			I2C3_PCLK_EN();
+		}
+	}else
+	{
+		if (pI2Cx == I2C1)
+		{
+			I2C1_PCLK_DI();
+		} else if (pI2Cx == I2C2)
+		{
+			I2C2_PCLK_DI();
+		} else if (pI2Cx == I2C3)
+		{
+			I2C3_PCLK_DI();
+		}
+	}
+}
